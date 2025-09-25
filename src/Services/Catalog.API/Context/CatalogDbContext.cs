a18468b0ff20a79494db019f33720c30506612de
asifanwar2 <asif.anwar@orbitax.com>
2025-09-25 11:57:34 +0600

    configured CatalogDbContext and implemented Repository Pattern

---
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        private static readonly IConfiguration Configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build()
+        
+        private static readonly string ConnectionString = Configuration.GetConnectionString("Catalog.API");
+
+        private static readonly string DatabaseName = Configuration.GetValue<string>("DatabaseName");
+        
+        public CatalogDbContext() : base(ConnectionString, DatabaseName)
+        {
+
+        }
+    }
+}
