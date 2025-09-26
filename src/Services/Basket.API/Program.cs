4d4afe8482090594f51a733784b4bca30f67ce4f
Asif Anwar <asif.anwar.sajid@gmail.com>
2025-09-27 04:53:25 +0600

    Connected to Redis cache database

--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = builder.Configuration.GetConnectionString("BasketDB");
+});
 
 var app = builder.Build();
 
